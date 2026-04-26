daf0b7027cd32fe0ddd7a5704d1677214e574e6b
Georg Guba <georg.guba@gmail.com>
2026-04-27 01:16:15 +0200

    migrate to hetzner's new cloud dns

--- config/90_swag_envsub.sh
+++ config/90_swag_envsub.sh
@@ -5,4 +5,4 @@ set -e
 sed -e "s|\${DOMAIN}|${URL}|g" /config/www/index.html.template > /config/www/index.html
 
 # Perform in-place substitution of Hetzner credentials in dns-conf.
-sed -i "s/^dns_hetzner_api_token.*/dns_hetzner_api_token = ${HETZNER_API_TOKEN}/" /config/dns-conf/hetzner.ini
+sed -i "s/^dns_hetzner_cloud_api_token.*/dns_hetzner_cloud_api_token = ${HETZNER_CLOUD_API_TOKEN}/" /config/dns-conf/hetzner-cloud.ini
